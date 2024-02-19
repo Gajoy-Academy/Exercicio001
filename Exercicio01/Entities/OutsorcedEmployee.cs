e81ed4ef15de694d2ae6da8bb94f186abfb08f99
ebagabe <gabriel.ebagabe@gmail.com>
2024-02-19 18:17:14 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicio01/Entities/OutsorcedEmployee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio01.Entities
+{
+    internal class OutsorcedEmployee : Employee
+    {
+        public double AdditionalCharge { get; set; }
+
+        public OutsorcedEmployee(){ }
+    }
+}
