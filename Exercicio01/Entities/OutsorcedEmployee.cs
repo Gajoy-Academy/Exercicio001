a78397a41c875ac6b86dafa91cec42587e8001fb
ebagabe <gabriel.ebagabe@gmail.com>
2024-02-19 19:06:34 -0300

    Exercise 01

--- Exercicio01/Entities/OutsorcedEmployee.cs
+++ Exercicio01/Entities/OutsorcedEmployee.cs
@@ -1,15 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Exercicio01.Entities
+﻿namespace Exercicio01.Entities
 {
     internal class OutsorcedEmployee : Employee
     {
         public double AdditionalCharge { get; set; }
 
-        public OutsorcedEmployee(){ }
+        public OutsorcedEmployee() { }
+
+        public OutsorcedEmployee(string name, int hours, double valuePerHour, double additionalCharge)
+            : base(name, hours, valuePerHour)
+        {
+            AdditionalCharge = additionalCharge;
+        }
+
+        public override double Payment()
+        {
+            return base.Payment() + 1.1 * AdditionalCharge;
+        }
     }
 }
--- Exercicio01/Program.cs
+++ Exercicio01/Program.cs
@@ -1,1 +1,38 @@
-﻿
+﻿using System.Globalization;
+using Exercicio01.Entities;
+
+
+List<Employee> list = new List<Employee>();
+
+Console.Write("Enter the number of employees: ");
+int n = int.Parse(Console.ReadLine());
+
+for(int i = 1; i <= n; i++)
+{
+    Console.WriteLine($"Employee #{i} data:");
+    Console.Write("Outsourced (y/n)? ");
+    char ch = char.Parse(Console.ReadLine());
+    Console.Write("Name: ");
+    string name = Console.ReadLine();
+    Console.Write("Hours: ");
+    int hours = int.Parse(Console.ReadLine());
+    Console.Write("Value per hours: ");
+    double valuePerHours = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+    if(ch == 'y')
+    {
+        Console.Write("Additional charge: ");
+        double additionalCharge = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        list.Add(new OutsorcedEmployee(name, hours, valuePerHours, additionalCharge));
+    }
+    else
+    {
+        list.Add(new Employee(name, hours, valuePerHours));
+    }
+}
+
+Console.WriteLine("\nPayments:");
+foreach(Employee emp in list)
+{
+    Console.WriteLine($"{emp.Name} - ${emp.Payment().ToString("F2", CultureInfo.InvariantCulture)}");
+}
